7f5ee489a1b8aa19a617b5d3f51640b828cbb35c
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-22 19:14:34 -0500

    2/22/23
    jay

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagment;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -19,13 +19,31 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        NewBlock();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         if (Done)
+         {
+            return;
+         }
+        var time = Mathf.Abs(Time.realtimeSinceStarup % 2f - 1f);
+        var pos1 = lastCube.transform.position + Vector3.up * 10f;
+        var pos2 = pos1 + ((Level % 2 == 0) ? Vector3.left : Vector3.forward) * 120;
+        if (Level % 2 == 0)
+        {
+            currentCube.transform.position = Vector3.Lerp(pos2, pos1, time);
+        }
+        else
+        {
+            currentCube.transform.position = Vector3.Lerp(pos1, pos2, time);
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            NewBlock();
+        }
     }
 
     void NewBlock()
@@ -37,14 +55,14 @@ public class GameController : MonoBehaviour
                 currentCube.transform.position.y,
                 Mathf.Round(currentCube.transform.position.z));
             
-            currentCube.transform.localscale = new Vector3(lastCube.transform.localscale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
-                                                            lastCube.transform.localscale.y,
-                                                           lastCube.transform.localscale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                                                            lastCube.transform.localScale.y,
+                                                           lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
             
             currentCube.transform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
 
-            if (currentCube.transform.localscale.x <= 0f ||
-                currentCube.transform.localscale.z <= 0f)
+            if (currentCube.transform.localScale.x <= 0f ||
+                currentCube.transform.localScale.z <= 0f)
                 {
                     Done = true;
                     text.gameObject.SetActive(true);
@@ -58,22 +76,15 @@ public class GameController : MonoBehaviour
     currentCube = Instantiate(lastCube);
     currentCube.name = Level + "";
     currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
+    Level ++;
+    Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
+    Camera.main.transform.LookAt(currentCube.transform.position);
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    }
+
+    IEnumerator X()
+    {
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("SampleScene");
     }
 }
